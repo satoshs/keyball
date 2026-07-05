cc5ece2757299f8575ad8a7fe45accdcf050c609
satoshs <68290124+satoshs@users.noreply.github.com>
2026-07-05 17:05:22 +0900

    Update keymap.c

--- qmk_firmware/keyboards/keyball/keyball44/keymaps/via/keymap.c
+++ qmk_firmware/keyboards/keyball/keyball44/keymaps/via/keymap.c
@@ -17,8 +17,215 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 */
 
 #include QMK_KEYBOARD_H
-
 #include "quantum.h"
+
+// コード表
+// 【KBC_RST: 0x5DA5】Keyball 設定のリセット
+// 【KBC_SAVE: 0x5DA6】現在の Keyball 設定を EEPROM に保存します
+// 【CPI_I100: 0x5DA7】CPI を 100 増加させます(最大:12000)
+// 【CPI_D100: 0x5DA8】CPI を 100 減少させます(最小:100)
+// 【CPI_I1K: 0x5DA9】CPI を 1000 増加させます(最大:12000)
+// 【CPI_D1K: 0x5DAA】CPI を 1000 減少させます(最小:100)
+// 【SCRL_TO: 0x5DAB】タップごとにスクロールモードの ON/OFF を切り替えます
+// 【SCRL_MO: 0x5DAC】キーを押している間、スクロールモードになります
+// 【SCRL_DVI: 0x5DAD】スクロール除数を１つ上げます(max D7 = 1/128)← 最もスクロール遅い
+// 【SCRL_DVD: 0x5DAE】スクロール除数を１つ下げます(min D0 = 1/1)← 最もスクロール速い
+
+////////////////////////////////////
+///
+/// 自動マウスレイヤーの実装 ここから
+/// 参考にさせていただいたページ
+/// https://zenn.dev/takashicompany/articles/69b87160cda4b9
+///
+////////////////////////////////////
+
+enum custom_keycodes
+{
+  KC_MY_BTN1 = KEYBALL_SAFE_RANGE, // Remap上では 0x5DAF
+  KC_MY_BTN2,                      // Remap上では 0x5DB0
+  KC_MY_BTN3                       // Remap上では 0x5DB1
+};
+
+enum click_state
+{
+  NONE = 0,
+  WAITING,   // マウスレイヤーが有効になるのを待つ。 Wait for mouse layer to activate.
+  CLICKABLE, // マウスレイヤー有効になりクリック入力が取れる。 Mouse layer is enabled to take click input.
+  CLICKING,  // クリック中。 Clicking.
+};
+
+enum click_state state; // 現在のクリック入力受付の状態 Current click input reception status
+uint16_t click_timer;   // タイマー。状態に応じて時間で判定する。 Timer. Time to determine the state of the system.
+
+uint16_t to_reset_time = 800; // この秒数(千分の一秒)、CLICKABLE状態ならクリックレイヤーが無効になる。 For this number of seconds (milliseconds), the click layer is disabled if in CLICKABLE state.
+
+const int16_t to_clickable_movement = 0; // クリックレイヤーが有効になるしきい値
+const uint16_t click_layer = 6;          // マウス入力が可能になった際に有効になるレイヤー。Layers enabled when mouse input is enabled
+
+int16_t mouse_record_threshold = 30; // ポインターの動きを一時的に記録するフレーム数。 Number of frames in which the pointer movement is temporarily recorded.
+int16_t mouse_move_count_ratio = 5;  // ポインターの動きを再生する際の移動フレームの係数。 The coefficient of the moving frame when replaying the pointer movement.
+
+int16_t mouse_movement;
+
+// クリック用のレイヤーを有効にする。　Enable layers for clicks
+void enable_click_layer(void)
+{
+  layer_on(click_layer);
+  click_timer = timer_read();
+  state = CLICKABLE;
+}
+
+// クリック用のレイヤーを無効にする。 Disable layers for clicks.
+void disable_click_layer(void)
+{
+  state = NONE;
+  layer_off(click_layer);
+}
+
+// 自前の絶対数を返す関数。 Functions that return absolute numbers.
+int16_t my_abs(int16_t num)
+{
+  if (num < 0)
+  {
+    num = -num;
+  }
+
+  return num;
+}
+
+// 自前の符号を返す関数。 Function to return the sign.
+int16_t mmouse_move_y_sign(int16_t num)
+{
+  if (num < 0)
+  {
+    return -1;
+  }
+
+  return 1;
+}
+
+// 現在クリックが可能な状態か。 Is it currently clickable?
+bool is_clickable_mode(void)
+{
+  return state == CLICKABLE || state == CLICKING;
+}
+
+bool process_record_user(uint16_t keycode, keyrecord_t *record)
+{
+
+  switch (keycode)
+  {
+  case KC_MY_BTN1:
+  case KC_MY_BTN2:
+  case KC_MY_BTN3:
+  {
+    report_mouse_t currentReport = pointing_device_get_report();
+
+    // どこのビットを対象にするか。 Which bits are to be targeted?
+    uint8_t btn = 1 << (keycode - KC_MY_BTN1);
+
+    if (record->event.pressed)
+    {
+      // ビットORは演算子の左辺と右辺の同じ位置にあるビットを比較して、両方のビットのどちらかが「1」の場合に「1」にします。
+      // Bit OR compares bits in the same position on the left and right sides of the operator and sets them to "1" if either of both bits is "1".
+      currentReport.buttons |= btn;
+      state = CLICKING;
+    }
+    else
+    {
+      // ビットANDは演算子の左辺と右辺の同じ位置にあるビットを比較して、両方のビットが共に「1」の場合だけ「1」にします。
+      // Bit AND compares the bits in the same position on the left and right sides of the operator and sets them to "1" only if both bits are "1" together.
+      currentReport.buttons &= ~btn;
+      enable_click_layer();
+    }
+
+    pointing_device_set_report(currentReport);
+    pointing_device_send();
+    return false;
+  }
+
+  default:
+    if (record->event.pressed)
+    {
+      disable_click_layer();
+    }
+  }
+
+  return true;
+}
+
+report_mouse_t pointing_device_task_user(report_mouse_t mouse_report)
+{
+  int16_t current_x = mouse_report.x;
+  int16_t current_y = mouse_report.y;
+
+  if (current_x != 0 || current_y != 0)
+  {
+
+    switch (state)
+    {
+    case CLICKABLE:
+      click_timer = timer_read();
+      break;
+
+    case CLICKING:
+      break;
+
+    case WAITING:
+      mouse_movement += my_abs(current_x) + my_abs(current_y);
+
+      if (mouse_movement >= to_clickable_movement)
+      {
+        mouse_movement = 0;
+        enable_click_layer();
+      }
+      break;
+
+    default:
+      click_timer = timer_read();
+      state = WAITING;
+      mouse_movement = 0;
+    }
+  }
+  else
+  {
+    switch (state)
+    {
+    case CLICKING:
+      break;
+
+    case CLICKABLE:
+      if (timer_elapsed(click_timer) > to_reset_time)
+      {
+        disable_click_layer();
+      }
+      break;
+
+    case WAITING:
+      if (timer_elapsed(click_timer) > 50)
+      {
+        mouse_movement = 0;
+        state = NONE;
+      }
+      break;
+
+    default:
+      mouse_movement = 0;
+      state = NONE;
+    }
+  }
+
+  mouse_report.x = current_x;
+  mouse_report.y = current_y;
+
+  return mouse_report;
+}
+
+////////////////////////////////////
+///
+/// 自動マウスレイヤーの実装 ここまで
+///
+////////////////////////////////////
 
 // clang-format off
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
