519c54c8cfcc83b286acfb29f1500676b08eba54
satoshs <68290124+satoshs@users.noreply.github.com>
2026-07-05 16:45:30 +0900

    Update keymap.c

--- qmk_firmware/keyboards/keyball/keyball44/keymaps/via/keymap.c
+++ qmk_firmware/keyboards/keyball/keyball44/keymaps/via/keymap.c
@@ -261,6 +261,27 @@ layer_state_t layer_state_set_user(layer_state_t state) {
     return state;
 }
 
+layer_state_t layer_state_set_user(layer_state_t state)
+{
+  // レイヤーが1または3の場合、スクロールモードが有効になる
+  keyball_set_scroll_mode(get_highest_layer(state) == 3);
+  // keyball_set_scroll_mode(get_highest_layer(state) == 1 || get_highest_layer(state) == 3);
+
+  // レイヤーとLEDを連動させる
+  uint8_t layer = biton32(state);
+  switch (layer)
+  {
+  case 6:
+    rgblight_sethsv(HSV_WHITE);
+    break;
+
+  default:
+    rgblight_sethsv(HSV_OFF);
+  }
+
+  return state;
+}
+
 #ifdef OLED_ENABLE
 
 #    include "lib/oledkit/oledkit.h"
